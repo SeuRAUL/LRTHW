341851eff7794f13725b51df588bf6acef6558d1
SeuRAUL <kenny.is.inmortal@gmail.com>
2013-05-31 11:24:26 -0300

    ex40 e testes

---
+++ teste/main.rb
@@ -0,0 +1,12 @@
+# require '~/LRTHW/teste/mystuff'
+require_relative 'mystuff'
+
+# MyStuff.apple()
+# puts MyStuff::TANGERINE
+
+
+# puts '-' * 10
+
+thing = MyStuff.new()
+thing.apple()
+puts thing.tangerine
