6af9dc834bc17e0e7275e4624769a863a751697b
SeuRAUL <seuraul.0@gmail.com>
2016-02-10 20:53:14 +0000

    edit ex40

--- ex40.rb
+++ ex40.rb
@@ -13,11 +13,17 @@ end
 
 happy_bday = Song.new(["Happy birthday to you",
 										"I don't want to get sued",
-										"So I'll stop roght there"])
+										"So I'll stop right there"])
 
 bull_on_parade = Song.new(["They rally around the family",
 													"With pockets full of shells"])
 
-happy_bday.sing_me_a_song()
+camarote = Song.new(["Como é que você ainda tem coragem de falar comigo?",
+                     "Além de não ter coração, não tem juízo",
+                     "Fez o que fez e vem me pedir pra voltar"])
 
-bull_on_parade.sing_me_a_song()
+#happy_bday.sing_me_a_song()
+
+#bull_on_parade.sing_me_a_song()
+
+camarote.sing_me_a_song()
