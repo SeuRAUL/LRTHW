17c5ea3963dc98a1846225bdbf0635de3801b21d
SeuRAUL <seuraul.0@gmail.com>
2016-02-26 22:45:51 +0000

    game engine and views

--- projects/gothonweb/bin/app.rb
+++ projects/gothonweb/bin/app.rb
@@ -1,4 +1,5 @@
 require 'sinatra'
+require './lib/gothonweb/map.rb'
 
 set :port, 8080
 set :bind, '0.0.0.0'
@@ -6,18 +7,36 @@ set :static, true
 set :public_folder, "static"
 set :views, "views"
 enable :sessions
+set :session_secret, 'BADSECRET'
 
 get '/' do
-  return 'Hello world'
+  session[:room] = 'START'
+  redirect to('/game')
 end
 
-get '/hello/' do
-  erb :hello_form
-end
-
-post '/hello/' do
-  greeting = params[:greeting] || "Hi There"
-  name = params[:name] || "Nobody"
+get '/game' do
+  room = Map::load_room(session)
   
-  erb :index, :locals => {'greeting' => greeting, 'name' => name }
+  if room
+    erb :show_room, :locals => {:room => room}
+  else
+    erb :you_died
+  end
+end
+
+post '/game' do
+  room = Map::load_room(session)
+  action = params[:action]
+  
+  if room
+    next_room = room.go(action) || room.go('*')
+    
+    if next_room
+      Map::save_room(session, next_room)
+    end
+    
+    redirect to('/game')
+  else
+    erb :you_died
+  end
 end
